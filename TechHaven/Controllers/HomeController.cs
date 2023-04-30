6b16d1193c2f6697bdf542af3efa466a78079ba9
EmirKapic <ekapic1@etf.unsa.ba>
2023-04-30 11:17:21 +0200

    dodan register/login view

--- TechHaven/Controllers/HomeController.cs
+++ TechHaven/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace TechHaven.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
