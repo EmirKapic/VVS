62e45495554e1c7013d54eeefa90f8d70f19274b
EmirKapic <ekapic1@etf.unsa.ba>
2023-05-02 18:22:25 +0200

    myAccount view osim wishlista

---
+++ TechHaven/Controllers/MyAccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechHaven.Controllers
+{
+    public class MyAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+        
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+    }
+}
