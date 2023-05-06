33aa831106c6dc5a492eda2c45711e88bd08c64e
EmirKapic <ekapic1@etf.unsa.ba>
2023-05-06 17:28:34 +0200

    model refactoring

--- TechHaven/Models/Recommendation.cs
+++ TechHaven/Models/Recommendation.cs
@@ -1,5 +1,8 @@
-﻿namespace TechHaven.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechHaven.Models
 {
+    [NotMapped]
     public class Recommendation
     {
         public Customer Customer { get; set; }
