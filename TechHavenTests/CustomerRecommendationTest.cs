3af83e80fec5728d52bc96de28e96a9361031387
unknown <ekapic1@etf.unsa.ba>
2023-11-25 14:36:59 +0100

    update

---
+++ TechHavenTests/CustomerRecommendationTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechHaven.Models;
+using TechHaven.Services;
+
+namespace TechHavenTests
+{
+    [TestClass]
+    public class CustomerRecommendationTest
+    {
+        public CustomerRecommendation recommendation;
+        [TestInitialize]
+        public void Setup()
+        {
+            recommendation = new CustomerRecommendation("fav", null);
+        }
+
+        [TestMethod]
+        public void TestRecommendation()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product(){Category = "fav", Id= 1},
+                new Product(){Category = "notfav", Id= 2},
+                new Product(){Category = "notfav", Id= 3},
+                new Product(){Category = "fav", Id=4}
+            };
+            recommendation._products = products;
+            var res = recommendation.RecommendProducts().ToArray();
+            Assert.AreEqual(2, res.Length);
+            Assert.AreEqual(1, res[0].Id);
+            Assert.AreEqual(4, res[1].Id);
+        }
+
+        [TestMethod]
+        public void TestNoneFound()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product(){Category = "notfav", Id= 1},
+                new Product(){Category = "notfav", Id= 2},
+                new Product(){Category = "notfav", Id= 3},
+                new Product(){Category = "notfav", Id=4}
+            };
+            recommendation._products = products;
+            var res = recommendation.RecommendProducts().ToArray();
+            Assert.AreEqual(0, res.Length);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullList()
+        {
+            recommendation._products = null;
+            var res = recommendation.RecommendProducts().ToArray();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullFavCategory()
+        {
+            recommendation._products = new List<Product>();
+            recommendation.favoriteCategory = null;
+            var res = recommendation.RecommendProducts().ToArray();
+        }
+
+    }
+}
--- TechHavenTests/HomeControllerTest.cs
+++ TechHavenTests/HomeControllerTest.cs
@@ -1,5 +1,9 @@
+using Castle.Core.Resource;
+using Microsoft.EntityFrameworkCore;
 using Moq;
+using System.Linq.Expressions;
 using TechHaven.Controllers;
+using TechHaven.Data;
 using TechHaven.Models;
 using TechHaven.Services;
 
@@ -9,9 +13,28 @@ namespace TechHavenTests
     public class HomeControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestRecommend()
         {
-            Assert.IsTrue(true);
+            var productList = new List<Product>();
+            productList.Add(new Product() { Model = "testModel1"});
+            productList.Add(new Product() { Model = "testModel2" });
+            productList.Add(new Product() { Model = "testModel3" });
+            var customerRecommendationMock = new Mock<CustomerRecommendation>("w", new List<Product>());
+            customerRecommendationMock.Setup(m => m.RecommendProducts()).Returns(productList);
+
+            var homeController = new HomeController(customerRecommendationMock.Object, null);
+            var res = homeController.recommendProduct();
+            Assert.AreEqual(3, res.Count());
+        }
+
+        [TestMethod]
+        public void TestRecommendWithNull()
+        {
+            List<Product> productList = null;
+            var customerRecommendationMock = new Mock<CustomerRecommendation>("w", new List<Product>());
+            customerRecommendationMock.Setup(m => m.RecommendProducts()).Returns(productList);
+            var homeController = new HomeController(customerRecommendationMock.Object, null);
+            Assert.AreEqual(0, homeController.recommendProduct().Count());
         }
     }
 }
