fd2d8e1b3b9bae0636ecd607b0b6d3a6483c8c11
zlendo1 <zlendo1@etf.unsa.ba>
2023-11-26 00:25:04 +0100

    added AlphabeticalStrategy tests

---
+++ TechHavenTests/AlphabeticalStrategyTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechHaven.Models;
+using TechHaven.Services;
+
+namespace TechHavenTests
+{
+    [TestClass]
+    public class AplhabeticalStrategyTest
+    {
+        [TestMethod]
+        public void SortProductsTest()
+        {
+            var productsOriginal = new List<Product>();
+
+            productsOriginal.Add(new Product { Manufacturer = "Volkswagen", Model = "Golf" });
+            productsOriginal.Add(new Product { Manufacturer = "Volkswagen", Model = "Polo" });
+            productsOriginal.Add(new Product { Manufacturer = "Volkswagen", Model = "Pasat" });
+            productsOriginal.Add(new Product { Manufacturer = "Toyota", Model = "Carola" });
+            productsOriginal.Add(new Product { Manufacturer = "Ford", Model = "Sedan" });
+
+            var productsExpected = productsOriginal.OrderBy(p => p.Manufacturer + p.Model).ToList();
+
+            CollectionAssert.AreEqual(productsExpected, new AlphabeticalStrategy().sortProducts(productsOriginal));
+        }
+    }
+}
