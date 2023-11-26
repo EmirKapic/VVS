ba6b54a30fc09392858c411823d10ffc55367754
Alem <amuratovic2@etf.unsa.ba>
2023-11-26 20:32:54 +0100

    Add LowestPriceStrategyTest

---
+++ TechHavenTests/LowestPriceStrategyTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechHaven.Models;
+using TechHaven.Services;
+
+namespace TechHavenTests
+{
+    [TestClass]
+    public class LowestPriceStrategyTest
+    {
+        [TestMethod]
+        public void SortProducts_returnSorted()
+        {
+            var productList = new List<Product>();
+            productList.Add(new Product() { Price = 150 });
+            productList.Add(new Product() { Price = 200 });
+            productList.Add(new Product() { Price = 100 });
+
+            var lowestPriceStrategy = new LowestPriceStrategy();
+            productList = lowestPriceStrategy.sortProducts(productList);
+            Assert.AreEqual(200, productList[2].Price);
+            Assert.AreEqual(150, productList[1].Price);
+            Assert.AreEqual(100, productList[0].Price);
+        }
+    }
+}
