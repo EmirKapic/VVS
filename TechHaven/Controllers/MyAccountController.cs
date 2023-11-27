603c0a353c0dc82bfb8a8b023f5a88a1acaea632
Muaz Sikiric <muaaz.sikiric@gmail.com>
2023-11-27 15:40:11 +0100

    Dodani testovi za MyAccount kontroler

--- TechHaven/Controllers/MyAccountController.cs
+++ TechHaven/Controllers/MyAccountController.cs
@@ -5,149 +5,57 @@ using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using TechHaven.Data;
 using TechHaven.Models;
+using TechHaven.Services;
 
 namespace TechHaven.Controllers
 {
     [Authorize(Roles = "Customer, Administrator")]
     public class MyAccountController : Controller
     {
-        private ApplicationDbContext _db;
-        private UserManager<Customer> _userManager;
-        private SignInManager<Customer> _signInManager;
-
-        public MyAccountController(ApplicationDbContext db, UserManager<Customer> userManager, SignInManager<Customer> signInManager)
+        private IProductManager _productManager;
+        private List<Product> _products;
+        
+        public MyAccountController(IProductManager productManager)
         {
-            _db = db;
-            _userManager = userManager;
-            _signInManager = signInManager;
+            _productManager = productManager;
+            _products = _productManager.GetAllProducts();
         }
-        [HttpGet]
-        public async Task<IActionResult> Index()
+
+        public IActionResult Index()
         {
-            /*[] imageArray = System.IO.File.ReadAllBytes(@"D:\TechHaven\TechHaven\wwwroot\images\Product Thumbnails\tablets2_img.jpg");
-            string base64image = Convert.ToBase64String(imageArray);
-
-            await _db.Image.AddAsync(new Image() { base64Content = base64image, Category = "Tablet" });
-            await _db.SaveChangesAsync();*/
-
             return View();
         }
 
-        public async Task<IActionResult> Order()
+        public IActionResult Wishlist()
         {
-            var usrId = _userManager.GetUserId(User);
-            var usr = await _db.Customer.Include(u => u.Orders).FirstAsync(u => u.Id == usrId);
-            if (usr.Orders != null)
+            if (!_products.Any())
             {
-                if (!usr.Orders.Any())
-                {
-                    return View(new List<Order>());
-                }
-                return View(usr.Orders);
+                return View(new List<Product>());
             }
-            return View();
+
+            return View(_products);
         }
         
-        public async Task<IActionResult> Wishlist()
+        public IActionResult AddToFavorites(int newProdId)
         {
-            var usrId = _userManager.GetUserId(User);
-            var usr = await _db.Customer.Include(u => u.Products).FirstAsync(u => u.Id == usrId);
-            if (usr.Products != null)
-            {
-                if (!usr.Products.Any())
-                {
-                    return View(new List<Product>());
-                }
-                return View(usr.Products);
-            }
-            return NotFound();
-        }
-
-        
-        public async Task<IActionResult> AddToFavorites([FromQuery] int newProdId)
-        {
-            var newProd = await _db.Product.FirstOrDefaultAsync(x => x.Id == newProdId);
-            var usrId = _userManager.GetUserId(User);
-            var usr = await _db.Customer.Include(u => u.Products).FirstAsync(u => u.Id == usrId);
-
-            if (usr == null) { return NotFound(); }
-
-            if (usr.Products.Contains(newProd)) { return Json(new { message = "Sucessfully added to favorites!" }); } //Zapravo bi trebao da izadje neki pop-up koji kaze already added to favorites
-
-            usr.Products.Add(newProd);
-            await _db.SaveChangesAsync();
-
-            return Json(new { message = "Sucessfully added to favorites!" });
-        }
-
-        
-        public async Task<IActionResult> DropFromFavorites(int id)
-        {
-            var usrId = _userManager.GetUserId(User);
-            var usr = await _db.Customer.Include(u => u.Products).FirstAsync(u => u.Id == usrId);
-            var prod = await _db.Product.FirstOrDefaultAsync(x => x.Id == id);
-            if (usr == null || usr.Products == null || prod == null) { return NotFound(); }
-            usr.Products.Remove(prod);
-            await _db.SaveChangesAsync();
-            return Json(new { message = "Sucessfully removed from favorites!" });
-		}
-
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Index(DataChangeViewModel newData, string change)
-        {
-            if (change == "data")
-            {
-                
-
-                var usr = await _userManager.GetUserAsync(User);
-                if (_db.Customer.Where(c => c.UserName == newData.Email).Any() && (await _userManager.GetUserNameAsync(usr) != newData.Email))
-                {
-                    ModelState.AddModelError("emptyFields", "Fields cannot be empty");
-                    return View(newData);
-                }
-                usr.FirstName = newData.FirstName;
-                usr.LastName = newData.LastName;
-                usr.Email = newData.Email;
-                usr.UserName = newData.Email;
-
-                await _userManager.UpdateAsync(usr);
-                await _db.SaveChangesAsync();
-                return Redirect(Request.Headers.Referer);
-            }
-            else if(change == "password")
-            {
-                if (newData.oldPass == null || newData.newPass == null || newData.repeatNew == null)
-                {
-                    ModelState.AddModelError("emptyPassFields", "Polja ne mogu biti prazna");
-                    return View(newData);
-                }
-                var usr = await _userManager.GetUserAsync(User);
-                if (!await _userManager.CheckPasswordAsync(usr, newData.oldPass))
-                {
-                    ModelState.AddModelError("oldPass", "Invalid password");
-                }
-                if (newData.newPass != newData.repeatNew)
-                {
-                    ModelState.AddModelError("repeatNew", "Passwords must match!");
-                }
-                if (!ModelState.IsValid)
-                {
-                    return View(newData);
-                }
-                await _userManager.ChangePasswordAsync(usr, newData.oldPass, newData.newPass);
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return NotFound();
-            }
+            var newProd = _products.FirstOrDefault(x => x.Id == newProdId);
             
+            if (_products.Contains(newProd))
+            { 
+                return Json("Product already in favorites!");
+            }
+
+            _products.Add(newProd);
+
+            return Json("Sucessfully added to favorites!");
         }
+        
+        public IActionResult DropFromFavorites(int prodId)
+        {
+            var prod = _products.FirstOrDefault(x => x.Id == prodId);
+            _products.Remove(prod);
+            
+            return Json("Sucessfully removed from favorites!");
+		}
     }
 }
---
+++ TechHavenTests/MyAccountControllerTest.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using TechHaven.Controllers;
+using TechHaven.Data;
+using TechHaven.Models;
+using TechHaven.Services;
+using static NuGet.Packaging.PackagingConstants;
+
+namespace TechHavenTests
+{
+    [TestClass]
+    public class MyAccountControllerTest
+    {
+        private Mock<ApplicationDbContext> _mockDbContext;
+        private ProductManager _productManager;
+        private MyAccountController controller;
+        private List<Product> _products;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            _products = new List<Product>
+            {
+                new Product { Id = 1, Category = "Laptop", Manufacturer = "HP", Model = "EliteBook", Price = 999.99, NumberOfAvailable = 10 },
+                new Product { Id = 2, Category = "Phone", Manufacturer = "Samsung", Model = "Galaxy S21", Price = 799.99, NumberOfAvailable = 20 },
+                new Product { Id = 3, Category = "Phone", Manufacturer = "Apple", Model = "iPhone 13", Price = 1399.99, NumberOfAvailable = 15 },
+                new Product { Id = 4, Category = "Tablet", Manufacturer = "Lenovo", Model = "Tab M9", Price = 299.99, NumberOfAvailable = 30 },
+            };
+
+            _mockDbContext = new Mock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
+            _mockDbContext.Setup(db => db.Product).Returns(DbSetMock.GetDbSetMock(_products).Object);
+            _productManager = new ProductManager(_mockDbContext.Object);
+
+            var ordersManagerMock = new Mock<IOrdersManager>();
+
+            controller = new MyAccountController(_productManager);
+        }
+
+        [TestMethod]
+        public void IndexTest()
+        {
+            var result = controller.Index();
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void WishlistTest()
+        {
+            var result = controller.Wishlist();
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            
+        }
+
+        [TestMethod]
+        public void WishlistWhenEmptyTest()
+        {
+            _products = new List<Product>();
+            _mockDbContext.Setup(db => db.Product).Returns(DbSetMock.GetDbSetMock(_products).Object);
+            _productManager = new ProductManager(_mockDbContext.Object);
+
+            var ordersManagerMock = new Mock<IOrdersManager>();
+
+            controller = new MyAccountController(_productManager);
+
+            var result = controller.Wishlist() as ViewResult;
+            CollectionAssert.AreEquivalent(new List<Product>(), result.ViewData.ToList());
+        }
+
+        [TestMethod]
+        public void AddToFavoritesTest()
+        {
+            var result = controller.AddToFavorites(5) as JsonResult;
+            var data = result.Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Sucessfully added to favorites!", data.ToString());
+        }
+
+        [TestMethod]
+        public void AddToFavoritesWhenAlreadyExistsTest()
+        {
+            var result = controller.AddToFavorites(1) as JsonResult;
+            var data = result.Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Product already in favorites!", data.ToString());
+        }
+
+        [TestMethod]
+        public void DropFromFavoritesTest()
+        {
+            var result = controller.DropFromFavorites(1) as JsonResult;
+            var data = result.Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Sucessfully removed from favorites!", data.ToString());
+        }
+    }
+}
